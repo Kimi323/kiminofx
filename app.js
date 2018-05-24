7cdd4480832d0169403a23fd32392d113936cc78
Kimi323 <wangmengya2010@yahoo.co.jp>
2018-05-25 00:52:15 +0900

    use express to serve pages

--- app.js
+++ app.js
@@ -1,30 +1,37 @@
 //const MongoClient = require('mongodb').MongoClient;
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
+const hbs = require('hbs');
 const MongoClient = require('mongodb').MongoClient;
+
+var app = express();
 
 app.use(bodyParser.urlencoded({extended: true}) );
 app.use(bodyParser.json());
-app.use('/html', express.static('html'));
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/views')); //__dirname is the path to root folder of this directory
 
-MongoClient.connect('mongodb://localhost:27017/fx', function(err, client) {
+MongoClient.connect('mongodb://localhost:27017/fx', (err, client) => {
     if (err) {
         return console.log('Unable to connect to MongoDB server');
     }
-    console.log('successfully connect to server');
+    console.log('successfully connect to MongoDB');
+
     app.get('/trade', (req, res) => {
-        res.send(`<h1>Hello world</h1>`);
         const db = client.db('fx');
         db.collection('trades').find({}).toArray().then((docs) => {
-            console.log(JSON.stringify(docs, undefined, 2));
+            res.send(docs);
+            // res.render('trade.hbs', {
+            //   monthlyResult: docs[0].amount
+            // });
         }, (err) => {
             console.log('unable to fetch trades', err);
         });
-        var server = app.listen(3000, function() {
-            var port = server.address().port;
-            console.log('Express server listening on port %s.', port);
-        });
-    })
+    });
+
+    var server = app.listen(3000, () => {
+        var port = server.address().port;
+        console.log('Express server listening on port %s.', port);
+    });
     //client.close();
 });
---
+++ views/trade.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    // $.ajax({
+    //     type: "GET",
+    //     url: "/trade",
+    //     contentType: 'application/json',
+    //     // data: JSON.stringify(result),
+    //     success: function(result){
+    //         console.log(result);
+    //         $('#list').html(result);
+    //     }
+    // });
+});
